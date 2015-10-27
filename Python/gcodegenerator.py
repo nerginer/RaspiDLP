75dc7f195e207fd1db5a7b73f595c6491538190b
Nuri Erginer <nerginer@gnexlab.com>
2015-10-27 21:01:54 +0200

    development first

--- Python/gcodegenerator.py
+++ Python/gcodegenerator.py
@@ -1,112 +1,35 @@
-import string
-import json
-import zipfile
-import shutil
+
 import os
-from sys import exit
-import logger
-from pubsub import pub
-
-
-print 'gcodeGenerator.py started'
-
-pub.sendMessage('Log', arg1='debug', arg2='*** gcodegenerator.py started ***')
-pub.sendMessage('Log', arg1='debug', arg2='*** ------------------------- ***')
+import time
+import sys
+import json
 
 settingsJsonFile="../json/printerSettings.json"
-#arguman olarak alacak
-meterialJsonFile="../json/meterial_b9.json"
 
 
 in_file = open(settingsJsonFile,"r")   
 settingsJson = json.load(in_file)
 in_file.close()
 
-in_file = open(meterialJsonFile,"r")   
-meterialJson = json.load(in_file)
-in_file.close()
 
 
-pub.sendMessage('Log', arg1='debug', arg2='json files loaded')
+print 'gcodeGenerator.py started'
+print 'Number of arguments:', len(sys.argv), 'arguments.'
+print 'Argument List:', str(sys.argv)
 
 
-
-
-#unzip input file
-def myUnZip(inputfile,outpath):
-    
-    #del outpath if exists
-    if os.path.exists(outpath):
-        shutil.rmtree(outpath)
-    #end
-
-    # open zip file
-
-    with zipfile.ZipFile(inputfile,"r") as z:
-        z.extractall(outpath)
-
-    # end   
-
-
-myInputFile = ""
-
-for file in os.listdir(settingsJson["download_dir"]):
-    if file.endswith(".zip"):
-        myInputFile=file
-
-if (myInputFile == ""):
- print ('no imput .zip file')
- #log here
- exit('no imput .zip file')
-
-#"/Users/gnexlab_imac/code/dlp_raspi/RaspiDLP/Python/Temp"
-pub.sendMessage('Log', arg1='debug', arg2='input file is: '+settingsJson["download_dir"]+'/'+myInputFile)
-
-#unzip image files
-myUnZip(settingsJson["download_dir"]+'/'+myInputFile,settingsJson["print_data_dir"])
-
-pub.sendMessage('Log', arg1='debug', arg2='input file unziped to: '+settingsJson["print_data_dir"])
-
-
-#number of image files
-number_of_slices = len([name for name in os.listdir(settingsJson["print_data_dir"]) if os.path.isfile(os.path.join(settingsJson["print_data_dir"], name))])
-
-pub.sendMessage('Log', arg1='debug', arg2='number number_of_slices: '+ str(number_of_slices))
-
-gcodelist = []
-
-gcodelist.append(settingsJson["header_code"])
-
-for c in range(1,int(number_of_slices)):
-
-    gcodelist.append(";<Slice> " + str(c))
-
-    if (c < int(meterialJson["number_of_bottom_layers"])):
-        gcodelist.append(";<Delay> " + meterialJson["cure_time_bottom_layers"])
-        gcodelist.append(";<Slice> Blank \r\n")
-        gcodelist.append(settingsJson["first_layer_lift_code"])
-        gcodelist.append(";<Delay> " + settingsJson["first_layer_motion_time"])
-        #gcodelist.append(";Wait for Motion Complated")
-    else:
-        gcodelist.append(";<Delay> " + meterialJson["cure_time_nominal_layers"])
-        gcodelist.append(";<Slice> Blank \r\n")
-        gcodelist.append(settingsJson["lift_code"])
-        gcodelist.append(";<Delay> " + settingsJson["layer_motion_time"])
-    
-
-gcodelist.append(settingsJson["footer_code"])
+settingsJsonFile=sys.argv[1]
+meterialJsonFile=sys.argv[2]
+printImgFile=sys.argv[3]
 
 gcodefile = open(settingsJson["print_data_dir"]+"/printGcode.gcode", 'w')
 
-for item in gcodelist:
-  gcodefile.write("%s\n" % item)
-  
+gcodefile.write("settingsJsonFile:%s\n" % settingsJsonFile)
+gcodefile.write("meterialJsonFile:%s\n" % meterialJsonFile)
+gcodefile.write("printImgFile:%s\n" % printImgFile)
+
 gcodefile.close()
 
-pub.sendMessage('Log', arg1='debug', arg2='gcode generated and saved to '+settingsJson["print_data_dir"]+"/printGcode.gcode")
+time.sleep(3)
 
-pub.sendMessage('Log', arg1='debug', arg2='*** gcodegenerator.py ended ***')
-#print "GCode List : ", gcodelist
-
-
-
+print 'gcode file generated'
--- Python/printManager.py
+++ Python/printManager.py
@@ -1,42 +1,14 @@
 from time import sleep
-import serial
-import pygame
-from pygame.locals import *
+
 import logger
 import json
 import os
 import sys
-from pubsub import pub
+
 from bayeux.bayeux_client import BayeuxClient
 from urllib2 import Request, urlopen
 
-pub.sendMessage('Log', arg1='debug', arg2='*** printManager.py started ***')
-pub.sendMessage('Log', arg1='debug', arg2='*** ------------------------- ***')
 
-settingsJsonFile="../json/printerSettings.json"
-
-in_file = open(settingsJsonFile,"r")   
-settingsJson = json.load(in_file)
-in_file.close()
-
-pub.sendMessage('Log', arg1='debug', arg2='json files loaded')
-
-pub.sendMessage('Log', arg1='debug', arg2='gameinit started')
-# Call this function so the Pygame library can initialize itself
-pygame.init()
-# Create an 1024x768 sized screen
-#screen = pygame.display.set_mode((1024, 768),FULLSCREEN) gercegi bu
-H = int(settingsJson["projector_res_h"])
-W = int(settingsJson["projector_res_w"])
-screen = pygame.display.set_mode((H,W))
-
-background_position = [0, 0]
-#Siyah Blank Screen
-myBlackScreen = pygame.Surface(screen.get_size())
-myBlackScreen = myBlackScreen.convert()
-myBlackScreen.fill((0,0,0))
-#Mouse gozukmesin
-pygame.mouse.set_visible(0)
 
 state = 'play'
 
@@ -45,9 +17,9 @@ def cb(data):
     print data['data']['text']
     state = data['data']['text']
 
-def doPrint(baseFileName):
+def doPrint():
 
-    pub.sendMessage('Log', arg1='debug', arg2='faye client started')
+  
 
     #faye client *********************
     client = BayeuxClient('http://localhost:8000/faye')
@@ -56,13 +28,14 @@ def doPrint(baseFileName):
     client.start()
     #faye client end *****************
 
-    fp=open(baseFileName,'r')
-    pub.sendMessage('Log', arg1='debug', arg2='gcode file opened')
+    
+  
 
-    lines=fp.readlines()
-    for line in lines:
+    
+    for line in range(0,10):
         #pause
         while (state == 'pause'):
+            print 'pause'
             pass
             #belki buraya siyeh ekran yaip dururum
 
@@ -71,44 +44,20 @@ def doPrint(baseFileName):
             print 'print canceled'
             return
 
-        if ';<Slice>' in line :
-            processSliceParam(line.split(' ')[1])
-        if ';<Delay>' in line :
-            processDelay(line.split(' ')[1])
-        if not';' in line :
-            processGCode(line)
+        showSlide(line)
+        processDelay(2)
             
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit ()
+      
 
-    pygame.quit ()
-
-# function gcode sender
-def processGCode(line):
-    if not line.isspace(): 
-       pass
-       # print 'GCode: '+line
-       # ser.write(line+'\n')
 
 
 # function to Delay
 def processDelay(var):
-    #print 'Delaying for druation: '+var
-    pygame.time.delay(int(var))
+    print 'Delaying for druation: '+str(var)
+    sleep(var)
 
-# function to processSliceParam
-def processSliceParam(var):
-    if 'Blank' in var :
-        showBlank()
-    else:
-    #if var.isdigit():
-        showSlide(var.rstrip('\r\n'))
-        
-#number of image files
-number_of_slices = len([name for name in os.listdir(settingsJson["print_data_dir"]) if os.path.isfile(os.path.join(settingsJson["print_data_dir"], name))])
-number_of_slices = number_of_slices -1
-#burayi duzelt
+
+
 
 def sendFayeMessage(myMessage):
     values = '{"channel":"/status","data":{"text":"'+myMessage+'"}}'
@@ -121,21 +70,17 @@ def sendFayeMessage(myMessage):
    
 
 def showSlide(var):
-    statusData = 'Layer: '+var+' of '+str(number_of_slices)
+    statusData = 'Layer: '+str(var)+' of 10'
     sendFayeMessage(statusData)
-    #print statusData
-    # Load and set up graphics.
-    background_image = pygame.image.load(settingsJson["print_data_dir"]+'/slice_'+var+'.png').convert()
-    screen.blit(background_image, background_position)
-    pygame.display.flip()
+    print statusData
+    
 
 def showBlank():
-    #print 'Showing Blank'
-    screen.blit(myBlackScreen, background_position)
-    pygame.display.flip()
+    print 'Showing Blank'
+    
 
 
         
-doPrint(settingsJson["print_data_dir"]+"/printGcode.gcode")
+doPrint()
 
 
